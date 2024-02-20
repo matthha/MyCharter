86111f0840ec8bfc3d6810ed5d2ae34ac59a8c4f
Matthew <matthha@umich.edu>
2024-02-20 10:30:59 -0500

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -79,6 +79,7 @@ function App(): React.JSX.Element {
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
+            This is my new text.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
